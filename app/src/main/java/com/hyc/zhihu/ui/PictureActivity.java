35ccd2a39534a6991180b5a3e8e812d7136ddbe9
jastrelax <yinpinjiu@gmail.com>
2016-05-12 17:22:33 +0800

    更新数据库

---
+++ app/src/main/java/com/hyc/zhihu/ui/PictureActivity.java
@@ -0,0 +1,15 @@
+package com.hyc.zhihu.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/5/12.
+ */
+public class PictureActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
